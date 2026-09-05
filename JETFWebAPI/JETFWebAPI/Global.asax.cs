398cd8952d2e6e86bf610284ed00131b27c485b6
only1335 <only1335@yahoo.com.tw>
2026-09-06 00:29:24 +0800

    新增 NLog 超過 30 天資料夾自動清理機制

--- JETFWebAPI/JETFWebAPI/Global.asax.cs
+++ JETFWebAPI/JETFWebAPI/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -11,7 +13,58 @@ namespace JETFWebAPI
     {
         protected void Application_Start()
         {
+            DeleteExpiredLogDirectories();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private static void DeleteExpiredLogDirectories()
+        {
+            const int retentionDays = 30;
+            string logDirectory = Path.Combine(HttpRuntime.AppDomainAppPath, "Logs");
+            DateTime cutoffDate = DateTime.Today.AddDays(-retentionDays);
+
+            try
+            {
+                if (!Directory.Exists(logDirectory))
+                {
+                    return;
+                }
+
+                foreach (string directoryPath in Directory.GetDirectories(logDirectory))
+                {
+                    string directoryName = Path.GetFileName(directoryPath);
+                    DateTime directoryDate;
+
+                    if (DateTime.TryParseExact(
+                        directoryName,
+                        "yyyy-MM-dd",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out directoryDate) && directoryDate < cutoffDate)
+                    {
+                        try
+                        {
+                            Directory.Delete(directoryPath, true);
+                        }
+                        catch (IOException)
+                        {
+                            // Keep folders that are still in use and retry next startup.
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            // Keep folders that the application cannot delete.
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                // Log cleanup must not prevent the application from starting.
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Log cleanup must not prevent the application from starting.
+            }
         }
     }
 }
